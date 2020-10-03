c2d5bbac332260d7e0efe9541339556573d2c05b
nirajGunawardana <47717506+nirajGunawardana@users.noreply.github.com>
2020-10-03 22:23:30 +0530

    post office details

---
+++ frontEnd/src/app/modules/admin-home/postoffice/postoffice.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface PeriodicElement {
+  position: number;
+  psname: string;
+  code: string;
+
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, psname: 'colombo', code: 'C01'   },
+  { position: 2, psname: 'gampaha', code: 'G02'  },
+  { position: 3, psname: 'kaluthara', code: 'K03'  },
+  { position: 4, psname: 'galle', code: 'G06'  },
+  { position: 5, psname: 'matara', code: 'M07'  },
+
+];
+
+@Component({
+  selector: 'app-postoffice',
+  templateUrl: './postoffice.component.html',
+  styleUrls: ['./postoffice.component.scss']
+})
+export class PostofficeComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  displayedColumns: string[] = ['position', 'psname', 'code' ];
+  dataSource = ELEMENT_DATA;
+}
