37d4dd5c3c509785599309540064d7cb7797770b
nirajGunawardana <47717506+nirajGunawardana@users.noreply.github.com>
2020-10-04 22:54:10 +0530

    change post office component

--- frontEnd/src/app/modules/admin-home/postoffice/postoffice.component.ts
+++ frontEnd/src/app/modules/admin-home/postoffice/postoffice.component.ts
@@ -4,7 +4,6 @@ export interface PeriodicElement {
   position: number;
   psname: string;
   code: string;
-
 }
 const ELEMENT_DATA: PeriodicElement[] = [
   { position: 1, psname: 'colombo', code: 'C01'   },
@@ -12,21 +11,18 @@ const ELEMENT_DATA: PeriodicElement[] = [
   { position: 3, psname: 'kaluthara', code: 'K03'  },
   { position: 4, psname: 'galle', code: 'G06'  },
   { position: 5, psname: 'matara', code: 'M07'  },
-
 ];
 
 @Component({
   selector: 'app-postoffice',
   templateUrl: './postoffice.component.html',
-  styleUrls: ['./postoffice.component.scss']
+  styleUrls: ['./postoffice.component.scss'],
 })
 export class PostofficeComponent implements OnInit {
-
   constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  displayedColumns: string[] = ['position', 'psname', 'code' ];
+  displayedColumns: string[] = ['position', 'psname', 'code','deleteEmployee' ];
   dataSource = ELEMENT_DATA;
 }
